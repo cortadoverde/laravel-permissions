919a717083bced9ce31fb14e988fbab8ceebb2c4
Pablo Adrian Samudia <p.a.samu@gmail.com>
2025-07-22 10:07:12 -0300

    Correccion path para las rutas case sensitive

--- src/LaravelPermissions.php
+++ src/LaravelPermissions.php
@@ -14,7 +14,7 @@ class LaravelPermissions extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/migrations');
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/Routes/web.php');
 
 
         if ($this->app->runningInConsole()) {
